5d9c6a5ac5a2112a4a808d02f198ddcf16172a34
Orlando Santos <Santos.Orlando17@gmail.com>
2023-10-31 01:08:56 -0700

    add base action for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-"https://github.com/Osantos17/Osantos17.github.io.git"
 export default defineConfig({
+  base: '/',
   plugins: [react()],
 })

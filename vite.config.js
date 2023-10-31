a8e542154d66598a94d9ee47b698af398722f3d6
Orlando Santos <Santos.Orlando17@gmail.com>
2023-10-31 00:49:51 -0700

    deploy actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+"https://github.com/Osantos17/Osantos17.github.io.git"
 export default defineConfig({
   plugins: [react()],
 })
